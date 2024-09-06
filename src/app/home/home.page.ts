995a6f64b3b25de0a35db6dcf27dff848dc619b7
davib <davi.brantes@gmail.com>
2024-09-06 15:20:32 -0300

    feat: formulario finalizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Platform } from '@ionic/angular';
+const IMG_DIR = "stored-image"
+
+interface LocalFile{
+  name: string;
+  path: string;
+  data: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,27 +17,42 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  cadastroForm: FormGroup;
+  photo: any;
+  dadosJSON: string | null = null;
 
-  nome: string = '';
-  cpf: string = '';
-  rg: string = '';
-  telefone: string = '';
-  endereco: string = '';
-  cnh: string = '';
-  photo: string = '';
-  login: string = '';
-  senha: string = '';
-
-  constructor() {}
-
-  cadastrar() {
-    const vistoriador = {
-      nome: this.nome,
-      // ... outros campos
-    };
-  
-    localStorage.setItem('vistoriador', JSON.stringify(vistoriador));
-    console.log('Dados salvos:', vistoriador);
+  constructor(private fb: FormBuilder) {
+    this.cadastroForm = this.fb.group({
+      nome: ['', Validators.required],
+      cpf: ['', Validators.required],
+      rg: ['', Validators.required],
+      telefone: ['', Validators.required],
+      endereco: [''],
+      cnh: ['', Validators.required],
+      login: ['', Validators.required],
+      senha: ['', Validators.required],
+      selfie: ['', Validators.required],
+    });
   }
 
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 100,
+      resultType: CameraResultType.DataUrl
+    });
+    this.photo = image;
+    this.cadastroForm.patchValue({ selfie: image.dataUrl });
+  }
+
+  onSubmit() {
+    if (this.cadastroForm.valid) {
+      const formData = this.cadastroForm.value;
+      this.dadosJSON = JSON.stringify(formData, null, 2);
+      console.log('Dados cadastrados:', formData);
+    }
+  }
+
+  exibirDadosJSON() {
+    alert(this.dadosJSON);
+  }
 }
